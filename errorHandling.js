d71bb4c762416521e0226bc2f2892d48eac7ba39
unknown <danialhydari@yahoo.com>
2022-08-30 21:04:54 +0500

    Recursive function for factorial added.

---
+++ errorHandling.js
@@ -0,0 +1,31 @@
+console.log("8Hello World");
+
+// // //function HAseebDanial(){
+// //     try{
+// //         console.log(" We are trying to catch an error ");
+// //         errorhappenedsuddenly;
+// //     }
+// //     catch(err) {
+// //         console.log("Error found" + err);
+// //     }
+// //     finally {
+// //         console.log("This is alwaysto be used");
+// //     }
+// // }
+
+// function helloAll() {
+   
+//     try {
+//     console.log("this is a Hello All Method");
+//     console.log("This function is still working");
+//     throw new Error("this is a Hello Error");
+    
+// }
+// catch(err) {
+//     console.log("Error Caught " + err);
+// }
+// finally {
+//     console.log("Always run this")
+// }
+// };
+// helloAll()
